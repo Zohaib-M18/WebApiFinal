1d8402cbb28b20610f6da5a5a4d73d5852018f02
Zohaib Hassan <Zohaib Hassan@DESKTOP-J9BP4EC>
2020-10-16 11:08:15 +0500

    uploded

---
+++ WebApiFinal/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPii.Controllers
+{
+    public class StudentController : ApiController
+    {
+        [HttpGet]
+        public int GetData1()
+        {
+            return 10;
+        }
+
+        [HttpGet]
+        public int GetData2(int a, int b) //Get data from query string
+        {
+            return a + b;
+        }
+
+        [HttpPost]
+        public String Save(StudentDTO dto) //Get data from body
+        {
+            return dto.name + "-testing";
+        }
+
+    }
+
+    public class StudentDTO
+    {
+        public int id { get; set; }
+        public String name { get; set; }
+    }
+}
